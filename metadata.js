3eac6a8ed4d7d678ac857ff3eb3f760b17eaf45f
Abimaelr <abimaelr.albu@gmail.com>
2021-10-19 10:20:27 -0300

    Turmas

---
+++ metadata.js
@@ -0,0 +1,46 @@
+module.exports =
+    {
+        "Fundamental 1": [
+            {nome: "Artes", ha: 2, cod: "f1"},
+            {nome: "Ciências", ha: 4, cod: "f1"},
+            {nome: "Educação Física", ha: 3, cod: "f1"},
+            {nome: "Ensino Religioso", ha: 1, cod: "f1"},
+            {nome: "Geografia", ha: 3, cod: "f1"},
+            {nome: "História", ha: 4, cod: "f1"},
+            {nome: "Língua Inglesa", ha: 2, cod: "f1"},
+            {nome: "Língua Portuguesa", ha: 6, cod: "f1"},
+            {nome: "Matemática", ha: 5, cod: "f1"}
+            ],
+        "Fundamental 2": [
+            {nome: "Artes", ha: 2, cod: "f2"},
+            {nome: "Ciências", ha: 4, cod: "f2"},
+            {nome: "Educação Física", ha: 3, cod: "f2"},
+            {nome: "Ensino Religioso", ha: 1, cod: "f2"},
+            {nome: "Geografia", ha: 3, cod: "f2"},
+            {nome: "História", ha: 4, cod: "f2"},
+            {nome: "Língua Inglesa", ha: 2, cod: "f2"},
+            {nome: "Língua Portuguesa", ha: 6, cod: "f2"},
+            {nome: "Matemática", ha: 5, cod: "f2"}
+        ],
+        "Anos Iniciais": [
+            {nome: "Artes", ha: 2, cod: "ai"},
+            {nome: "Ciências", ha: 3, cod: "ai"},
+            {nome: "Educação Física", ha: 3, cod: "ai"},
+            {nome: "Ensino Religioso", ha: 1, cod: "ai"},
+            {nome: "Geografia", ha: 3, cod: "ai"},
+            {nome: "História", ha: 4, cod: "ai"},
+            {nome: "Língua Portuguesa", ha: 5, cod: "ai"},
+            {nome: "Matemática", ha: 5, cod: "ai"}
+        ],
+        "EJA 1° Segmento":  [
+            {nome: "Artes", ha: 1, cod: "eja1"},
+            {nome: "Ciências", ha: 1, cod: "eja1"},
+            {nome: "Educação Física", ha: 3, cod: "eja1"},
+            {nome: "Ensino Religioso", ha: 1, cod: "eja1"},
+            {nome: "Geografia", ha: 3, cod: "eja1"},
+            {nome: "História", ha: 4, cod: "eja1"},
+            {nome: "Língua Inglesa", ha: 2, cod: "eja1"},
+            {nome: "Língua Portuguesa", ha: 6, cod: "eja1"},
+            {nome: "Matemática", ha: 5, cod: "eja1"}
+        ],
+    }
