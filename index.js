5508773330dc4c50b5cea22132b1cf1f31f6b7fb
Abimael <abimaelr.albu@gmail.com>
2021-11-12 09:11:16 -0300

    port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/', dashboard);
 app.use('/classes', classes);
 app.use('/disciplinas', disciplinas);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port , ()=>{
     console.log('Ativo')
