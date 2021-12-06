64d6952598e2adb421fd3d5520b14847ecf7ad7b
Abimael <abimaelr.albu@gmail.com>
2021-12-06 15:03:28 -0300

    changes

--- models/connect.js
+++ models/connect.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const MONGO_DB_URL =  process.env.MONGO_DB_URL || 'mongodb://localhost:27017/diarioEscolar';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/diarioEscolar';
 // process.env.MONGO_DB_URL ||
 const OPTIONS = {
     useNewUrlParser: true,
@@ -10,12 +10,12 @@ const OPTIONS = {
 
 let db = null;
 
-const connection = () => (db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+const connection = () => (db ?
+    Promise.resolve(db) :
+    MongoClient.connect(MONGO_DB_URL, OPTIONS)
     .then((conn) => {
-    db = conn.db('diarioEscolar');
-    return db;
+        db = conn.db('diarioEscolar');
+        return db;
     }));
 
 module.exports = connection;
